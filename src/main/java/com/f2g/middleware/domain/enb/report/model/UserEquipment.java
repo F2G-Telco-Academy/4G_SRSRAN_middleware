59336a2fb958454bce871bfd4ed3e60da622d23c
Adrien Monthe <montheadrien@gmail.com>
2024-05-23 21:46:12 +0100

    feat:  add ue attributes in json

--- src/main/java/com/f2g/middleware/domain/enb/report/model/UserEquipment.java
+++ src/main/java/com/f2g/middleware/domain/enb/report/model/UserEquipment.java
@@ -1,5 +1,6 @@
 package com.f2g.middleware.domain.enb.report.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
@@ -8,4 +9,6 @@ import lombok.experimental.FieldNameConstants;
 @Setter
 @FieldNameConstants
 public class UserEquipment {
+    @JsonProperty("ue_rnti")
+    private Integer ueRnti;
 }
